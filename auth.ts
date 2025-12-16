1c7727460dbaae3111e19bed417b8ef936c7b0d3
Felipe <ossogamesbr1@gmail.com>
2025-12-16 17:43:30 -0300

    fix(auth): expose user id

--- auth.ts
+++ auth.ts
@@ -14,4 +14,18 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+      return token
+    },
+    session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+      }
+      return session
+    },
+  },
 });
